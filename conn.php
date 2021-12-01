c9912f07f76d444b439f339b506f96cf69c1cd6a
Nguyễn Xích Long <nguyenlong010520@gmail.com>
2021-12-02 03:49:22 +0700

    Add conn.php

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "nd";
+$con = mysqli_connect($host,$user,$password,$database);
+if (mysqli_connect_errno()){
+    echo "connectio Fail :".mysqli_connect_errno();exit;
+}
+?>
